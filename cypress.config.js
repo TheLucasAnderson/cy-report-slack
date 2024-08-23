55e7f88fcb5618185d512bb6b3275210fd3848e3
TheLucasAnderson <lucasand96@gmail.com>
2024-08-23 01:43:44 -0300

    feat: Add mochawesome as reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,16 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    screenshotsFolder: 'cypress/screenshots',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: true,
+    html: false,
+    json: true,
   },
 });
