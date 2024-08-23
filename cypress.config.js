3daf30bfce315a6b99e9845214d4501817354fe8
TheLucasAnderson <lucasand96@gmail.com>
2024-08-23 13:17:01 -0300

    feat: Edit cypress yml add new job and commands

--- cypress.config.js
+++ cypress.config.js
@@ -2,16 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    screenshotsFolder: 'cypress/screenshots',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'cypress/results',
-    overwrite: true,
-    html: false,
-    json: true,
+    video: false,
+    reporter: 'json',
+    reporterOptions: {
+      output: 'cypress',
+      outputFile: 'result.json',
+    },
   },
 });
